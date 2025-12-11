99337e67ecb2f798746467d2acdc8603700641c0
asejik <asejik@gmail.com>
2025-12-11 17:04:31 +0100

    forced email service

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'; // Added useEffect
+import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { db } from '../services/firebase';
 import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
